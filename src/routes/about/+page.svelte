3d6b146b72dd55b484e2ab8a83f578ad36779ec5
Joanna Marowa <marojm1@student.op.ac.nz>
2024-10-15 11:20:08 +1300

    Add the header tcomponent to the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-
+import Header from "../../lib/Header.svelte";
 </script>
 
-<html>
-<p>Here is naty </p>
-</html>
+<Header headingTitle ="About page"/>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus odit ut veritatis voluptatum expedita accusamus blanditiis dolor ducimus, minima amet obcaecati, nesciunt doloremque officiis unde nulla nisi minus eum natus.</p>
+
 
 <style>
 
