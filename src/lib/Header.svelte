33743a399770f2880852921425e2deb6c488fc48
marojm1 <marojm1@student.op.ac.nz>
2024-10-15 10:54:16 +1300

    display the header component on page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,17 @@
 <script>
-
 </script>
 
 <header>
-
+  <h1>
+    <nav>
+      <ul>
+          <li><a  href="/" >Home</a></li>
+          <li><a href ="/">About</a></li>
+          <li><a href="/">Contact</a></li>
+      </ul>
+    </nav>
+  </h1>
 </header>
 
 <style>
-    
 </style>
