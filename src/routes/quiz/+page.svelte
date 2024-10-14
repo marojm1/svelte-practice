7523d78c6b1f4138c704b34b0445b20bc3e18b2e
Joanna Marowa <marojm1@student.op.ac.nz>
2024-10-15 12:52:25 +1300

    config quiz

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -4,3 +4,15 @@ import Header from "$lib/Header.svelte"
 </script>
 
 <Header headingTitle = "Dog matching Quiz"/>
+
+<h3>Start the quiz to be one step closer to finding your perfect canine</h3>
+
+<div>
+    <form action="dog.quiz">
+        <label for= "fname">First Name</label>
+        <input type="text" id ="fname" name ="firstname" placeholder="Your name">
+
+        <label for="lname">Last Name</label>
+        <input type="text" id ="lname" name = "lastname" placeholder="Your last name">
+    </form>
+</div>
