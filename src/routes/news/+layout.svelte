cf7ef252170d20b75818e785eda25548d54ca810
Joanna Marowa <marojm1@student.op.ac.nz>
2024-10-22 11:21:01 +1300

    adjust to have a side bar

---
+++ src/routes/news/+layout.svelte
@@ -0,0 +1,19 @@
+<div>
+    
+    <aside>
+        <p>
+            This only a news page
+            <a href="src\routes\news\article\+page.svelte">Article</a>
+        </p>
+    </aside>
+</div>
+
+<slot></slot>
+<style>
+    div{
+        display:flex;
+    }
+  aside {
+    background-color: #aaa;
+  }
+</style>
