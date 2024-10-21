531ff69fa1ae962b15137f34d7d925693d312d13
Joanna Marowa <marojm1@student.op.ac.nz>
2024-10-22 11:20:32 +1300

    add a news article

---
+++ src/routes/news/article/+page.svelte
@@ -0,0 +1,25 @@
+<h2>This is a news article</h2>
+
+<article>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo numquam
+    nesciunt neque eum corporis iusto, illum reprehenderit omnis iure
+    voluptatibus, eaque rerum eveniet ad. Asperiores voluptas aliquid molestias
+    quibusdam doloribus.
+  </p>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora illo
+    architecto aliquam explicabo dolores odio officia quasi ut nam, delectus et
+    nisi unde sequi, incidunt quam. Dignissimos libero tempora numquam!
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus sint,
+    laborum, distinctio perspiciatis voluptate, saepe quas tempore fuga dolor
+    ipsam aliquid optio vel in labore incidunt qui atque sit repudiandae.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum velit
+    exercitationem dolorem maiores amet quam ullam voluptatibus culpa, mollitia
+    veritatis ipsam saepe dignissimos earum qui harum ex eum aliquid impedit!
+  </p>
+</article>
