4857cff97dd8277bc53b3b042569d7f556dc973e
Joanna Marowa <marojm1@student.op.ac.nz>
2024-10-22 10:46:38 +1300

    make a basic layout file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<slot/>
+
+<footer>This should be on every page, since it is in the layout file</footer>
+
+<style>
+    footer{
+        padding :1em;
+        background-color: #aaa
+        ;
+    }
+</style>
